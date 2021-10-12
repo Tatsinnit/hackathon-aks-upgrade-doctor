324a3fad19e4576e9403ad5785b3929773e48a44
Tatsinnit <tamishra@microsoft.com>
2021-10-12 21:59:26 +1300

    Add rest of logic for PDB via pseudo code.

--- aks/upgrade/poddistributionbudget.go
+++ aks/upgrade/poddistributionbudget.go
@@ -6,14 +6,15 @@ import (
 	"log"
 	"os"
 	"path"
+	"reflect"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
 
 	dirname, err := os.UserHomeDir()
 	if err != nil {
@@ -32,12 +33,12 @@ func main() {
 	// Creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		fmt.Println(fmt.Errorf("Getting access to K8S failed: %w", err))
+		fmt.Println(fmt.Errorf("getting access to K8S failed: %w", err))
 	}
 
 	namespacesList, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		fmt.Println(fmt.Errorf("Unable to list namespaces in the cluster: %w", err))
+		fmt.Println(fmt.Errorf("unable to list namespaces in the cluster: %w", err))
 	}
 	for _, namespace := range namespacesList.Items {
 
@@ -50,6 +51,57 @@ func main() {
 			fmt.Println("Min Available : ", i.Spec.MinAvailable)
 			fmt.Println("Max Available : ", i.Spec.MaxUnavailable)
 			fmt.Println("DisruptionsAllowed : ", i.Status.DisruptionsAllowed)
+
+			// The non-zero value for ALLOWED DISRUPTIONS means that the disruption controller has seen the pods, counted the matching pods, and updated the status of the PDB.
+			if i.Status.DisruptionsAllowed == 0 && i.Spec.MaxUnavailable.String() == "0" {
+				fmt.Println("Upgrade operation will fail - you are requiring zero voluntary evictions, so cannot successfully drain a Node running one of the Pods")
+			}
+
+			// Below code tries to implement following pseudo code
+			// 	Count = Count of (kubectl get pods --selector=<Labels provided in PDB>)
+			// 	if (Count != 0)
+			// 		{
+			// 			If (Count - "MIN AVAILABLE" == 0)
+			// 			{
+			// 				Printf("Upgrade operation will fail - cannot successfully drain a Node running one of the Pods")
+			//
+			// 			}
+			// 		}
+			// Note: Above pseudo code implemenation below
+
+			if i.Status.DisruptionsAllowed == 0 && i.Spec.MinAvailable.String() > "0" {
+				podlist, err := GetPods(clientset, namespace.Name)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+				count := 0
+				for _, pod := range podlist.Items {
+					if reflect.DeepEqual(pod.Labels, i.Labels) {
+						count = count + 1
+					}
+				}
+				if count != 0 {
+					diff := count - i.Spec.MinAvailable.IntValue()
+					if diff == 0 {
+						fmt.Println("Upgrade operation will fail - cannot successfully drain a Node running one of the Pods")
+					}
+				}
+			}
+
 		}
 	}
+}
+
+func GetPods(clientset *kubernetes.Clientset, namespace string) (*v1.PodList, error) {
+	// Create a pod interface for the given namespace
+	podInterface := clientset.CoreV1().Pods(namespace)
+
+	// List the pods in the given namespace
+	podList, err := podInterface.List(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		return nil, fmt.Errorf("getting pods failed: %w", err)
+	}
+
+	return podList, nil
 }
