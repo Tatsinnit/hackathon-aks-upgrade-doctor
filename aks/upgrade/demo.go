9c5d5997a8b3ab9db730fc0c3488342f959c3723
Peter Bomber <peter.bomber@microsoft.com>
2021-10-15 12:27:24 +1300

    WIP: subnet check

--- aks/upgrade/demo.go
+++ aks/upgrade/demo.go
@@ -79,6 +79,7 @@ func createEngineDemoCommand() *cobra.Command {
 				clusterCtx,
 				rules.RulesSet{
 					upgradePDBRuleProvider,
+					subnetCapacityRuleProvider,
 					rules.NewRule(
 						"upgrade/armtest-managed-cluster",
 						func(
--- aks/upgrade/subnet.go
+++ aks/upgrade/subnet.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/network/mgmt/network"
+	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/Tatsinnit/hackathon-aks-upgrade-doctor/pkg/rules"
 )
 
@@ -19,6 +21,71 @@ var subnetCapacityRuleProvider = rules.NewRule(
 		//     * we can follow this demo to construct the client: https://github.com/Azure-Samples/azure-sdk-for-go-samples/blob/b49c4162aa1d96bc2b1b42afecbf4a21b420e568/network/subnets.go#L18-L24
 		// 4. calculate the capacity of the subnet and calculate result
 
-		return nil, nil
+		clusterInfo, err := clusterCtx.GetManagedClusterInformation(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		auth, err := clusterCtx.GetAzureAuthorizer()
+		if err != nil {
+			return nil, err
+		}
+
+		clusterModel, err := clusterInfo.GetLatestModel(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		subnetsClient := network.NewSubnetsClient(clusterInfo.GetSubscriptionID())
+		subnetsClient.Authorizer = auth
+
+		vnetsClient := network.NewVirtualNetworksClient(clusterInfo.GetSubscriptionID())
+		vnetsClient.Authorizer = auth
+
+		var results []*rules.CheckResult
+		for _, apProfile := range *clusterModel.AgentPoolProfiles {
+			ap, err := clusterInfo.GetAgentPoolInformation(ctx, to.String(apProfile.Name))
+			if err != nil {
+				return nil, err
+			}
+
+			apModel, err := ap.GetLatestModel(ctx)
+			if err != nil {
+				return nil, err
+			}
+
+			// TODO: Get the subnet object from *apModel.VnetSubnetID
+			// Problem: that value is nil. Can't see a way to tell the go client to retrieve it.
+			// From the rest API docs (https://docs.microsoft.com/en-us/rest/api/aks/agent-pools/get#agentpool)
+			// it looks like this property will be populated if the customer BYOs their own subnet....
+			// So may have to fall back to picking a vnet/subnet from the cluster node pool based on
+			// agent pool name, if the value is not specified.
+
+			// Try looping through the vnets in the node resource group
+			vnetsResult, err := vnetsClient.List(ctx, clusterInfo.GetNodeResourceGroup())
+			if err != nil {
+				return nil, err
+			}
+
+			for _, vnet := range vnetsResult.Values() {
+				// Does subnet name match agent pool name?
+				subnetsClient.Get(ctx, clusterInfo.GetNodeResourceGroup(), *vnet.Name, *apModel.Name, "")
+				// for _, subnet := vnet.Subnets {
+				// 	subnetsClient.Get(ctx, clusterInfo.GetNodeResourceGroup(), *vnet.Name, ag, "")
+				// }
+			}
+
+			// results = append(results, &rules.CheckResult{
+			// 	RuleID:   "upgrade/armtest-agent-pool",
+			// 	Category: category,
+			// 	Description: fmt.Sprintf(
+			// 		"Got subnet from agent pool: %s - subnetid: %s",
+			// 		*apProfile.Name,
+			// 		*apModel.VnetSubnetID,
+			// 	),
+			// })
+		}
+
+		return results, nil
 	},
 )
